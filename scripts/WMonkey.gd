b5e08766aacd47eb8b1199ecb585e78551d6a5d7
Cristian Morataya <cmorataya@develsystems.com>
2017-01-11 15:22:21 -0600

    Add git plugin and movement to the monkeys

--- scripts/WMonkey.gd
+++ scripts/WMonkey.gd
@@ -1,12 +1,23 @@
 extends KinematicBody2D
 
 export var gravity = 4000
-var velocity = Vector2()
+var velocity = Vector2(0,1)
+const WALK_SPEED = 200
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	velocity.y = delta * gravity
+	
+	if(Input.is_action_pressed("wm_jump")):
+		velocity.x = WALK_SPEED
+	
 	var motion = velocity * delta
 	motion = move(motion)
+	
+	if(is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
